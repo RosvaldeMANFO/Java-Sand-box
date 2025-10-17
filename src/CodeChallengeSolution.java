4847b1ea3c6de497f218673820b786dee188c5c0
Rosvadle MANFO <florientmanfo3@outlook.fr>
2025-10-17 23:50:36 +0200

    Feat: add solution fore some common technical test

---
+++ src/CodeChallengeSolution.java
@@ -0,0 +1,71 @@
+import kotlin.collections.builders.MapBuilder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+public class CodeChallengeSolution {
+
+    /**
+     * This function tell if a word is a palindrome;
+     * it means that we still have the same word after
+     * reversing characters.
+     * @param str String
+     * @return boolean
+     */
+    public boolean isPalindrome(String str) {
+        StringBuilder revertedStr = new StringBuilder();
+        for (char chr : str.toCharArray()) {
+            revertedStr.insert(0, chr);
+        }
+        return revertedStr.toString().equals(str);
+    }
+
+    /**
+     * This function tempts to determinate if
+     * all string arguments are anagrams or not.
+     * For that, it will return true if all arguments
+     * match the anagram constraint and false if not.
+     * @param args String
+     * @return boolean
+     */
+    public boolean areAnagrams(String... args) {
+        String[] sortedString = new String[args.length];
+
+        for (int i =0; i<args.length; i++) {
+            String str = args[i].replace("[^A-Za-z]+", "").toLowerCase();
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+
+            StringBuilder builder = new StringBuilder();
+            for(char ch: charArray){
+                builder.append(ch);
+            }
+            sortedString[i] = builder.toString();
+        }
+
+        return Arrays.stream(sortedString).allMatch(sortedString[0]::equals);
+    }
+
+    /**
+     * This fonction print a pyramid of a specific character with
+     * a given size.
+     * @param size int
+     * @param character char
+     */
+    public void printPyramidOf(int size, char character) {
+        int column = (2 * size) - 1;
+        int middle = Math.floorDiv(column, 2);
+        for(int i = 0; i < size; i++){
+            StringBuilder str = new StringBuilder();
+            for(int j= 0; j < column; j++){
+                if(j >= middle-i && j <= (middle + i)){
+                    str.append(character);
+                } else {
+                    str.append(" ");
+                }
+            }
+            System.out.println(str);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ public class Main {
     public static void main(String[] args){
 //        DiceRoller diceRoller = new DiceRoller();
 //        diceRoller.rollDice();
-        AudioPlayer audioPlayer = new AudioPlayer("src/assets/audio-sample.wav");
-        audioPlayer.on();
+//        AudioPlayer audioPlayer = new AudioPlayer("src/assets/audio-sample.wav");
+//        audioPlayer.on();
+        CodeChallengeSolution ccs = new CodeChallengeSolution();
+        System.out.println(ccs.isPalindrome("KAYAK"));
+        System.out.println(ccs.areAnagrams("They see", "The eyes"));
+        ccs.printPyramidOf(9, '*');
     }
 }
